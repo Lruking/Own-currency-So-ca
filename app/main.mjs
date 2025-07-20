c5555bed4d6514969d46eefdf9fdc0d5f04965f4
Lruking <107301713+Lruking@users.noreply.github.com>
2025-07-20 09:36:31 +0900

    Update main.mjs

--- app/main.mjs
+++ app/main.mjs
@@ -1,1 +1,18 @@
+import { Client, GatewayIntentBits } from 'discord.js';
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.once('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', message => {
+  if (message.author.bot) return;
+  if (message.content === '!ping') {
+    message.channel.send('Pong!');
+  }
+});
+
+// Koyebの環境変数は process.env.TOKEN に入る
+client.login(process.env.TOKEN);
 
